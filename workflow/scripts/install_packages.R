e81840507a641fc8e760d9ac7962bd3ff748d2e0
Steve Pederson <stephen.pederson.au@gmail.com>
2022-04-28 16:23:10 +0930

    Set last pre-BioC commit as fixed for extraChIPs

--- workflow/scripts/install_packages.R
+++ workflow/scripts/install_packages.R
@@ -3,12 +3,11 @@ success <- args[[1]]
 
 pkgs <- c(
   "tidyverse", "magrittr", "rtracklayer", "glue", "pander", "scales",
-  "plyranges", "yaml", "UpSetR", "BiocParallel", "scico", "extraChIPs",
-  "DiagrammeR", "steveped/spBioUtils", "Gviz", "Rsamtools", "GenomicInteractions",
-  "cowplot", "GenomeInfoDb", "ngsReports", "csaw", "edgeR", "quantro",
-  "qsmooth", "statmod", "IHW", "ggrepel", "rlang", "ggside", "InteractionSet",
-  "reactable", "htmltools", "VennDiagram", "Biostrings", "GenomicRanges",
-  "steveped/extraChIPs"
+  "plyranges", "yaml", "UpSetR", "BiocParallel", "scico", "DiagrammeR", 
+  "steveped/spBioUtils", "Gviz", "Rsamtools", "GenomicInteractions", "cowplot", 
+  "GenomeInfoDb", "ngsReports", "csaw", "edgeR", "quantro", "qsmooth", 
+  "statmod", "IHW", "ggrepel", "rlang", "ggside", "InteractionSet",  
+  "reactable", "htmltools", "VennDiagram", "Biostrings", "GenomicRanges"
 )
 
 pks2inst <- setdiff(pkgs, rownames(installed.packages()))
@@ -19,5 +18,14 @@ BiocManager::install(pks2inst,  update = TRUE, ask = FALSE, force = FALSE)
 stopifnot(
   all(basename(pkgs) %in% rownames(installed.packages()))
 )
+
+remotes::install_github(
+  "steveped/extraChIPs", 
+  ## This is the last commit before shifting to R4.2
+  ref = "de3e53111a93d664311a11c2c185ea3286d2a9a7", 
+  force = FALSE
+)
+
+stopifnot("extraChIPs" %in% rownames(installed.packages()))
 
 file.create(success)
