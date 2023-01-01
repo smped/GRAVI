d2d844c96c1e81cbacd518f6b566d21c059edbae
Steve Pederson <stephen.pederson.au@gmail.com>
2023-01-01 23:59:59 +1030

    Fixed package update script (really)

--- workflow/scripts/update_extrachips.R
+++ workflow/scripts/update_extrachips.R
@@ -2,10 +2,9 @@ args <- commandArgs(TRUE)
 f <- args[[1]]
 reqd_version <- args[[2]]
 
-cat("Checking for extraChIPs >= v", version, "\n")
+cat("Checking for extraChIPs >= v", reqd_version, "\n")
 inst_version <- packageVersion("extraChIPs")
 cat("Installed version is ", inst_version, "\n")
-
 
 needs_update <- inst_version < reqd_version
 cat(ifelse(needs_update, "Updating...", "No update required"))
