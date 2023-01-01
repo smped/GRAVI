9729347142706179ad3525e417c4a952cbb7bfd3
Steve Pederson <stephen.pederson.au@gmail.com>
2023-01-02 02:02:15 +1030

    Bugfix in make_greylist.R

--- workflow/scripts/make_greylist.R
+++ workflow/scripts/make_greylist.R
@@ -1,16 +1,14 @@
 library(readr)
 library(GenomeInfoDb)
 library(GreyListChIP)
+library(Rsamtools)
 
 args <- commandArgs(TRUE)
-bam <- args[[1]] ## Needs to be the full filename with .bam suffix
-sq_file <- args[[2]]
+bam <- BamFile(args[[1]])
+sq <- read_rds(args[[2]])
 bed <- args[[3]]
 
-sq <- read_rds(sq_file)
-
 ## This is set for only a single input file
-ip <- file.path(bam_path, "Input", bam)
 gl <- new("GreyList", karyotype = sq)
 gl <- countReads(gl, bam)
 set.seed(100)
