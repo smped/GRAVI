b582d8133f9ca6de976ea3bfd8155defa0bc7c6e
Steve Pederson <stephen.pederson.au@gmail.com>
2023-01-01 23:58:18 +1030

    Fixed package update script

--- workflow/scripts/update_extrachips.R
+++ workflow/scripts/update_extrachips.R
@@ -1,13 +1,17 @@
 args <- commandArgs(TRUE)
 f <- args[[1]]
-version <- args[[2]]
+reqd_version <- args[[2]]
 
-cat("Checking for extraChIPs >= v", version)
-needs_update <- packageVersion("extraChIPs") >= version
+cat("Checking for extraChIPs >= v", version, "\n")
+inst_version <- packageVersion("extraChIPs")
+cat("Installed version is ", inst_version, "\n")
+
+
+needs_update <- inst_version < reqd_version
 cat(ifelse(needs_update, "Updating...", "No update required"))
 if (needs_update) BiocManager::install("extraChIPs", ask = FALSE)
 
-success <- packageVersion("extraChIPs") >= version
+success <- packageVersion("extraChIPs") >= reqd_version
 stopifnot(success)
 
 file.create(f)
