30da20c8a4d26dac46deb82e21f6a665ef7f28a9
Steve Pederson <stephen.pederson.au@gmail.com>
2023-01-02 02:08:37 +1030

    Bugfix2 in make_greylist.R

--- workflow/scripts/make_greylist.R
+++ workflow/scripts/make_greylist.R
@@ -1,18 +1,21 @@
 library(readr)
 library(GenomeInfoDb)
 library(GreyListChIP)
-library(Rsamtools)
 
 args <- commandArgs(TRUE)
-bam <- BamFile(args[[1]])
+bam <- args[[1]]
 sq <- read_rds(args[[2]])
 bed <- args[[3]]
 
 ## This is set for only a single input file
 gl <- new("GreyList", karyotype = sq)
+cat("Counting reads...\n")
 gl <- countReads(gl, bam)
+cat("Calculating thresholds...\n")
 set.seed(100)
 gl <- calcThreshold(gl)
+cat("Making greylist...\n")
 gl <- makeGreyList(gl)
-
+cat("Writing to ", bed, "\n")
 export(gl, bed)
+cat("done\n")
