1c02b7517b7530a229aa5a657a2e4fff5e094cec
Steve Pederson <stephen.pederson.au@gmail.com>
2023-01-03 02:07:17 +1030

    Started adding network plots

--- workflow/scripts/custom_functions.R
+++ workflow/scripts/custom_functions.R
@@ -43,3 +43,48 @@ str_sep_to_title <- function(
       panel.grid = element_blank()
     )
 }
+
+#' Create a tbl_graph object from goseq results
+#'
+#' @details
+#' Distances between genesets are created by using the overlap-coefficient
+#'
+#' @param res data.frame with goseq results
+#' @param gs List of gene-sets with gene ids with each element. Names are gene-sets
+#' @param min_dist remove any edges with distances < min_dist
+#' @param alpha Threshold for filtering goseq results
+#'
+make_tbl_graph <- function(res, gs, min_dist = 0.9, alpha = enrich_alpha) {
+  res <- dplyr::filter(res, adj_p < alpha)
+  if (nrow(res) == 0) return(tbl_graph())
+  nm <- res$gs_name
+  combs <- combn(nm, 2)
+  d <- lapply(
+    seq_len(ncol(combs)),
+    function(i) {
+      pair <- combs[,i]
+      all <- unlist(gs[pair])
+      n <- min(vapply(gs[pair], length, integer(1)))
+      ## Use the overlap coefficient
+      ol <- sum(table(all) == 2) / n
+      1 - ol
+    }
+  )
+  edges <- combs %>%
+    t() %>%
+    set_colnames(c("from", "to")) %>%
+    as_tibble() %>%
+    mutate(d = unlist(d)) %>%
+    dplyr::filter(d < min_dist)
+  ## If there is a distance of zero, take the most highly ranked
+  omit <- dplyr::filter(edges, d == 0)$to
+  nodes <- tibble(id = seq_along(nm), label = nm) %>%
+    dplyr::filter(!id %in% omit) %>%
+    left_join(res, by = c("label" = "gs_name")) %>%
+    mutate(prop = numDEInCat / numInCat)
+  edges <- dplyr::filter(edges, d > 0)
+  node_ids <- setNames(nodes$id, nodes$label)
+  edges$from <- node_ids[edges$from]
+  edges$to <- node_ids[edges$to]
+  tbl_graph(nodes = nodes, edges = edges, directed = FALSE)
+}
